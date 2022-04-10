f6c8326d76a747788cb2721cfaf7fdeb51441915
Fernandes Joaquim <64234688+joaquimjfernandes@users.noreply.github.com>
2022-04-10 01:56:38 +0100

    Initial commit

---
+++ script/to_do.js
@@ -0,0 +1,35 @@
+document.querySelector('#push').onclick = function
+(){
+    if(document.querySelector('#newtasks input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML 
+        += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtasks input').value}
+                </span>
+                <button class="delete">
+                    <img src="trash.svg">
+                </button>
+            </div> 
+        `;
+
+        let current_tasks = document.querySelectorAll(".delete");
+        for(let i=0; i<current_tasks.length; i++) {
+            current_tasks[i].onclick = function() {
+                this.parentNode.remove();
+            }
+        }
+
+        let tasks = document.querySelectorAll(".task");
+        for(let i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+       
+        document.querySelector("#newtask input").value = "";
+    }
+}
